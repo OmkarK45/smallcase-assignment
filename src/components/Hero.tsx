0bc98a2e126300a5c5bf6f19fd24698b29d67906
Omkar <omkarak45@gmail.com>
2022-04-25 22:28:13 +0530

    feat: hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+export function Hero() {
+	return (
+		<div>
+			<div className="max-w-7xl mx-auto py-8 px-4 sm:py-16 sm:px-6 lg:px-8">
+				<div className="text-center">
+					<h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+						Smallcase Assignment
+					</h2>
+					<p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+						Search Flight Records...
+					</p>
+					<p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+						Type atleast 3 characters to start searching.
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}

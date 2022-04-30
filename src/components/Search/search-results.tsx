14262aed87e943ef31779ba199e4c209a79202e0
Omkar <omkarak45@gmail.com>
2022-04-30 22:48:39 +0530

    feat: search result component

---
+++ src/components/Search/search-results.tsx
@@ -0,0 +1,17 @@
+import { Package } from '~/types/package'
+import { ResultItem } from './result-item'
+
+interface Props {
+	packages: Package[]
+}
+
+export function SearchResults({ packages }: Props) {
+	return (
+		<ul role="list" className="space-y-3 ">
+			{packages.length > 0 &&
+				packages.map((item, index) => {
+					return <ResultItem package={item} key={index} href={'/something'} />
+				})}
+		</ul>
+	)
+}
